c6ee737e07d9027a66e8d1a21e317db73d5e30e8
campisano <riccardo.campisano@gmail.com>
2019-11-19 08:21:31 -0300

    fix: test use of docker to run tests

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 apt-get -q update
-apt-get -q -y install git gcc g++ make cmake libssl-dev libcurl4-openssl-dev
+apt-get -q -y install git gcc g++ make cmake libssl1.0-dev libcurl4-openssl-dev
 make -e release
--- ci/test.sh
+++ ci/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 apt-get -q update
-apt-get -q -y install libssl1.0.0 libcurl3
+apt-get -q -y install libssl1.0.2 libcurl3
 ./run.sh release test
