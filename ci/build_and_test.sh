e0171dc59ae057c76658a10fae04dd3989906dc6
campisano <riccardo.campisano@gmail.com>
2019-12-08 16:37:40 -0300

    feature:    cleanup CI scripts

---
+++ ci/build_and_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o errtrace -o pipefail
+
+# vars
+export DOCKER_FROM_IMAGE=$(./ci/custom/get_docker_from_image.sh)
+
+# build and test
+docker pull "${DOCKER_FROM_IMAGE}"
+docker run --mount type=bind,source="$(pwd)",target=/repository "${DOCKER_FROM_IMAGE}" /bin/bash -c 'cd /repository; ./ci/custom/internal_build.sh'
+docker run --mount type=bind,source="$(pwd)",target=/repository "${DOCKER_FROM_IMAGE}" /bin/bash -c 'cd /repository; ./ci/custom/internal_test.sh'
