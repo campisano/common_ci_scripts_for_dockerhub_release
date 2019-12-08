8ea0fdf024b218d85fe4986f7fbc1db7f8f36344
campisano <riccardo.campisano@gmail.com>
2019-12-08 18:19:12 -0300

    feature:    clean apt cache after install

--- ci/custom/internal_build.sh
+++ ci/custom/internal_build.sh
@@ -4,4 +4,5 @@ set -o errtrace -o errexit -o nounset -o pipefail
 
 apt-get -q update
 apt-get -q -y install gcc g++ make cmake libssl1.0-dev libcurl4-openssl-dev
+apt-get clean
 make -e release
--- ci/custom/internal_test.sh
+++ ci/custom/internal_test.sh
@@ -4,4 +4,5 @@ set -o errtrace -o errexit -o nounset -o pipefail
 
 apt-get -q update
 apt-get -q -y install libssl1.0.2 libcurl3
+apt-get clean
 ./run.sh release test
