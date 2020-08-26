98750af1f455359b7404515c4d58179555ec37be
campisano <riccardo.campisano@gmail.com>
2020-08-26 02:08:33 -0300

    feature:    add docker support inside CI docker images, switch from curl to wget in CI script due to curl issue to resume downloads

--- ci/custom/internal_coverage.sh
+++ ci/custom/internal_coverage.sh
@@ -11,7 +11,7 @@ apt-get -qq -y update
 apt-get -qq -y install --no-install-recommends apt-utils > /dev/null
 apt-get -qq -y install --no-install-recommends libssl1.0.2 libcurl3 > /dev/null
 apt-get -qq -y install --no-install-recommends gcc g++ make cmake libssl1.0-dev libcurl4-openssl-dev > /dev/null
-apt-get -qq -y install --no-install-recommends curl git lcov > /dev/null
+apt-get -qq -y install --no-install-recommends wget git lcov > /dev/null
 
 make -e debug
 
@@ -27,7 +27,8 @@ ls -alt --color
 lcov --remove coverage.info "/usr/*" "`pwd`/external/*" "`pwd`/src/test/*" --output-file coverage.info
 lcov --list coverage.info
 
-bash <(curl -s https://codecov.io/bash) -f coverage.info -t "${COVERAGE_TOKEN}"
+wget -c -nv --no-check-certificate -O codecov https://codecov.io/bash
+bash ./codecov -X coveragepy -X xcode -f coverage.info -t "${COVERAGE_TOKEN}"
 
 rm -f coverage.info
 make distclean
