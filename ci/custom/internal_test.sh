ce9be9c56046211b5989115abe83a1c4e3695065
campisano <riccardo.campisano@gmail.com>
2021-07-10 02:34:10 -0300

    feature:    ci - test cache

--- ci/custom/internal_test.sh
+++ ci/custom/internal_test.sh
@@ -2,14 +2,13 @@
 
 set -x -o errexit -o nounset -o pipefail
 
-export DEBIAN_FRONTEND=noninteractive
-
 mkdir -p .custom_cache/var/cache/apt
 cp -aT .custom_cache/var/cache/apt /var/cache/apt
 rm -f /etc/apt/apt.conf.d/docker*
 
+export DEBIAN_FRONTEND=noninteractive
 apt-get -qq -y update
-apt-get -qq -y install --no-install-recommends apt-utils > /dev/null
+apt-get -qq -y install --no-install-recommends apt-utils &> /dev/null
 apt-get -qq -y install --no-install-recommends libssl1.0.2 libcurl3 > /dev/null
 
 mv -n /var/cache/apt/* .custom_cache/var/cache/apt/
