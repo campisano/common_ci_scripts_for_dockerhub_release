be7b9df6f919c78231511af95e6ef1615925be78
campisano <riccardo.campisano@gmail.com>
2022-06-17 02:27:03 +0200

    feature:    multiarch - test separate build step with specific arch images

--- ci/custom/internal_test.sh
+++ ci/custom/internal_test.sh
@@ -2,8 +2,11 @@
 
 set -x -o errexit -o nounset -o pipefail
 
-mkdir -p .custom_cache/var/cache/apt
-cp -aT .custom_cache/var/cache/apt /var/cache/apt
+CACHE_DIR=/srv/cache
+BUILD_DIR=/srv/build
+
+mkdir -p ${CACHE_DIR}/var/cache/apt
+cp -aT ${CACHE_DIR}/var/cache/apt /var/cache/apt
 rm -f /etc/apt/apt.conf.d/docker*
 
 export DEBIAN_FRONTEND=noninteractive
@@ -11,6 +14,7 @@ apt-get -qq -y update
 apt-get -qq -y install --no-install-recommends apt-utils &> /dev/null
 apt-get -qq -y install --no-install-recommends libssl1.0.2 libcurl3 > /dev/null
 
-mv -n /var/cache/apt/* .custom_cache/var/cache/apt/
+mv -n /var/cache/apt/* ${CACHE_DIR}/var/cache/apt/
 
+cp -a ${BUILD_DIR}/* .
 ./run.sh release test
