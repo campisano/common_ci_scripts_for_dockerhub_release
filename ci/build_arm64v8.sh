03095d1d79db78db2aa6e0082ea45d5a23072e2f
campisano <riccardo.campisano@gmail.com>
2022-06-18 03:15:55 +0200

    feature:    multiarch - test docker buildx with multi-stage build

--- ci/build_arm64v8.sh
+++ ci/build_arm64v8.sh
@@ -11,16 +11,16 @@ docker pull "${DOCKER_IMAGE}"
 # build code isolatedly
 docker buildx ls
 docker buildx create --use
-docker buildx build --platform=linux/arm64/v8 --tag tmp_image --file - .  << EOF
-FROM --platform=linux/arm64/v8 ${DOCKER_IMAGE}
+docker buildx build --platform=linux/arm64/v8 --file - --output type=local,dest=out .  << EOF
+FROM ${DOCKER_IMAGE} as build
 COPY ${CACHE_DIR}/. /srv/cache
 COPY . /srv/repo
 RUN bash -c "cd /srv/repo; ./ci/custom/internal_build.sh"
+
+FROM scratch as artifact
+COPY --from=build /srv/build/. /build
+COPY --from=build /srv/cache/. /cache
 EOF
 
-CONTAINER=$(docker create --platform linux/arm64/v8 tmp_image)
-
-docker cp ${CONTAINER}:/srv/cache/. ${CACHE_DIR}
-docker cp ${CONTAINER}:/srv/build/. ${BUILD_DIR}
-docker rm --volumes ${CONTAINER}
-docker image rm tmp_image
+mv -n out/cache/* ${CACHE_DIR}
+mv -n out/build/* ${BUILD_DIR}
